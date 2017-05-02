759ee51a7e67345a3c371c1af4ce64186f97bdc0
Michael Chukwunyelu <mikemadu@yahoo.com>
2017-05-02 17:58:56 +0800

    Working on Crypto

--- model/ApplicantModel.cs
+++ model/ApplicantModel.cs
@@ -8,58 +8,7 @@ namespace BizApp.model
   public  class ApplicantModel
 
     {
-        //public List<Applicant> GetApplicantList()
-        //{
-        //    List<Applicant> lst = new List<Applicant>();//declare a list of Applicant variable
-
-        //   Applicant app;
-        //    app= new Applicant();//make a new applicant and populate the properties
-
-        //    app.Lastname = "Chuks";
-        //    app.Firstname = "Micky";
-        //    app.MiddleName = "C.";
-        //    app.Phone = "926800876";
-        //    app.Job = "Electronics Engineer";
-        //    app.id = 1;
-        //    app.key = Guid.NewGuid();
-        //    lst.Add(app);//add to the list
-
-        //    app = new Applicant();//make a new applicant and populate the properties
-
-        //    app.Lastname = "Santos";
-        //    app.Firstname = "Vicky";
-        //    app.MiddleName = "P.";
-        //    app.Phone = "927453789";
-        //    app.Job = "Mechanical Engineer";
-        //    app.id = 2;
-        //    app.key = Guid.NewGuid();
-        //    lst.Add(app);//add to the list
-
-        //    app = new Applicant();//make a new applicant and populate the properties
-
-        //    app.Lastname = "Petronelo";
-        //    app.Firstname = "John";
-        //    app.MiddleName = "W.";
-        //    app.Phone = "933008877";
-        //    app.Job = "Accountant";
-        //    app.id = 3;
-        //    app.key = Guid.NewGuid();
-        //    lst.Add(app);//add to the list
-
-        //    app = new Applicant();//make a new applicant and populate the properties
-
-        //    app.Lastname = "Escardo";
-        //    app.Firstname = "Marilyn";
-        //    app.MiddleName = "M.";
-        //    app.Phone = "09725447863";
-        //    app.Job = "Store Keeper";
-        //    app.id = 4;
-        //    app.key = Guid.NewGuid();
-        //    lst.Add(app);//add to the list
-
-        //    return lst;
-        //}
-
+     
         public List<Applicant> GetDemoApplicants()
         {
             List<Applicant> lst = new List<Applicant>();//declare a list of Applicant variable
--- model/LoginModel.cs
+++ model/LoginModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace BizApp.model
@@ -11,7 +12,7 @@ namespace BizApp.model
         {
             //In a real application, we will authenticate the user
             //and return true or false 
-            //For our demo purposes, let's just return true if username and password contain something;
+            //For our demo purposes, let//s just return true if username and password contain something;
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
             {
                 return false;
@@ -22,5 +23,88 @@ namespace BizApp.model
             }
 
         }
+
+
+        //public class PasswordHasher
+        //{
+
+        //    public const int SALT_BYTE_SIZE = 24;
+        //    public const int HASH_BYTE_SIZE = 24;
+
+        //public const int SALT_INDEX  = 1;
+        ///// <summary>
+        ///// Creates a hashed password from plain text. Salt is created and attached to the end of the password.
+        ///// </summary>
+        ///// <param name="plainTextPass">Plain text password to be hashed</param>
+        ///// <returns>Hashed password with salt appended</returns>
+        //    public static string CreateHashedPassword(string plainTextPass)  {
+        //        // Generate a random salt
+        //        var csprng = new RNGCryptoServiceProvider();
+
+        //        byte salt[SALT_BYTE_SIZE];
+        //    csprng.GetBytes(salt);
+
+        //    //Hash the password and encode the parameters
+        //        byte[] hash = HashIt(plainTextPass, salt, 1000, HASH_BYTE_SIZE);
+        //        return Convert.ToBase64String(hash) & ":" & Convert.ToBase64String(salt);
+
+        //}
+        //    /// <summary>
+        //    /// Will validate a password against its hash
+        //    /// </summary>
+        //    /// <param name="password">plain text password</param>
+        //    /// <param name="correctHash">correct hash</param>
+        //    /// <returns>true if hash is a representation of plain password</returns>
+        //    public static bool ValidatePassword(string password, string correctHash) {
+        //        //Extract the parameters from the hash
+        //        try
+        //        {
+        //            var delimiter = cchar(":");
+        //            string[] splited = correctHash.Split(delimiter);
+        //            var iterations = 1000;
+        //            byte[] hash = Convert.FromBase64String(splited[0]);
+        //        var salt As Byte() = Convert.FromBase64String(splited(SALT_INDEX))
+
+
+        //        byte[] testHash = HashIt(password, salt, iterations, hash.Length);
+        //      if (Convert.ToBase64String(hash) == Convert.ToBase64String(testHash){
+        //                return true;
+        //            }else{
+        //                return false;
+        //            }
+               
+                   
+               
+        //        }
+        //        catch (Exception)
+        //        {
+        //            return false;
+
+        //        }
+
+
+
+        //    }
+
+        ///// <summary>
+        ///// Computes the PBKDF2-SHA1 hash of a password.
+        ///// </summary>
+        ///// <param name="password"></param>
+        ///// <param name="salt"></param>
+        ///// <param name="iterations"></param>
+        ///// <param name="outputBytes"></param>
+        ///// <returns>A hash of the password.</returns>
+        ///// <remarks></remarks>
+        //    private static byte[] HashIt(string password, byte[] salt, int iterations, int outputBytes)
+        //    {
+        //        var pbkdf2_ = new Rfc2898DeriveBytes(password, salt);
+        //        pbkdf2_.IterationCount = iterations;
+        //        return pbkdf2_.GetBytes(outputBytes);
+        //    }
+
+
+        //}
+
+
     }
 }
